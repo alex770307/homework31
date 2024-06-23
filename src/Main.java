c5d8f812ad90020788c86507ba6855a9b68b3d47
alex_ <alex_7377@hotmail.com>
2024-06-23 12:34:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Что вы хотите что положить в Message ?");
+        String answer = scanner.nextLine();
+
+        if (answer.equals("строку")) {
+            System.out.println("Введите сообщение:");
+            String message = scanner.nextLine();
+            Message<String> stringMessage = new Message<>(message);
+            System.out.println("Содержимое сообщения :\n" + stringMessage.getContent());
+
+        } else if (answer.equals("число")) {
+            System.out.println("Введите число:");
+            int number = Integer.parseInt(scanner.nextLine());
+            Message<Integer> intMessage = new Message<>(number);
+            System.out.println("Содержимое сообщения :\n" + intMessage.getContent());
+        } else {
+            System.out.println("Повторите попытку !");
+        }
+    }
+}
---
+++ src/Message.java
@@ -0,0 +1,16 @@
+public class Message<T> {
+
+    private T content;
+
+    public Message(T content) {
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+}
