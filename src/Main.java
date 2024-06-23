0b3745eae4080928c8b7f08704b26dc09787e778
alex_ <alex_7377@hotmail.com>
2024-06-23 13:09:11 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Что вы хотите что положить в Message ?");
+        System.out.println("Что вы хотите положить в Message ?");
         String answer = scanner.nextLine();
 
         if (answer.equals("строку")) {
