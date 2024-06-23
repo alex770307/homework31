4c30a4c61b62fe3603064131c2d07181c99eb960
alex_ <alex_7377@hotmail.com>
2024-06-23 12:43:35 +0200

    first commit

--- src/Message.java
+++ src/Message.java
@@ -1,9 +1,24 @@
+import java.util.Objects;
+
 public class Message<T> {
 
     private T content;
 
     public Message(T content) {
         this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message<?> message = (Message<?>) o;
+        return Objects.equals(content, message.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(content);
     }
 
     public T getContent() {
